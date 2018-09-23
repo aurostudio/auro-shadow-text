2f460ac9b618d434c73fbfcbb3d4b07c1ab2c54f
Kien Vo <kien.vo@aurostudio.com>
2018-09-22 23:28:34 -0600

    Enhance pubspec.yaml to be qualified by dart analyzer

--- lib/auro_shadow_text.dart
+++ lib/auro_shadow_text.dart
@@ -5,8 +5,7 @@ import 'package:flutter/services.dart';
 export 'src/ShadowText.dart';
 
 class AuroShadowText {
-  static const MethodChannel _channel =
-      const MethodChannel('auro_shadow_text');
+  static const MethodChannel _channel = const MethodChannel('auro_shadow_text');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
